aa209412e0b7232167a6c66c296892e51ae66f99
ylu021 <yichen_0@hotmail.com>
2017-05-25 15:00:42 -0400

    split into presentational and container

--- client/src/components/popular.js
+++ client/src/components/popular.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+const langs = ['All', 'JS', 'Ruby', 'Java', 'Python', 'HTML', 'CSS']
+
 class Popular extends Component {
   constructor(props) {
     super(props)
@@ -16,23 +18,30 @@ class Popular extends Component {
   }
 
   render() {
-    const langs = ['All', 'JS', 'Ruby', 'Java', 'Python', 'HTML', 'CSS']
     return (
-      <ul className="lang-list">
-        {langs.map((lang, idx) => {
-          return (
-            <li
-              key={ idx }
-              onClick={ this.updateLang(lang) }
-              style= { lang === this.state.selectedLang ? { color: 'tomato' } : null }
-            >
-              { lang }
-            </li>
-          )
-        })}
-      </ul>
-    );
+      <div>
+        <Selected selectedLang={ this.state.selectedLang } onSelect={ this.updateLang } />
+      </div>
+    )
   }
+}
+
+function Selected (props) {
+  return (
+    <ul className="lang-list">
+      {langs.map((lang, idx) => {
+        return (
+          <li
+            key={ idx }
+            onClick={ props.onSelect(lang) }
+            style= { lang === props.selectedLang ? { color: 'tomato' } : null }
+          >
+            { lang }
+          </li>
+        )
+      })}
+    </ul>
+  )
 }
 
 export default Popular;
